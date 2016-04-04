ae5f97f56cebd271e4115d2a7dcbb536d7ed62ff
Benjamin Haoui <Benjamin@MacBook-Pro-de-Benjamin.local>
2016-04-04 22:48:10 +0200

    motif size

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const app = require('app');
 const BrowserWindow = require(('browser-window'));
 app.on('ready', function() {
   var mainWindow = new BrowserWindow({
-    width: 850,
+    width: 870,
     height: 800
   });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
